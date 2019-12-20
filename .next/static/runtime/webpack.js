b054cbf8330ef84c91ee6a1c986f3381cdd4c98d
Geovanny Mero <jefferson_mero1911@hotmail.com>
2019-12-20 00:20:29 -0500

    primer commit

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5a2eef2ad80973294197";
+/******/ 	var hotCurrentHash = "4c52fea99bc7913177a6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,50 @@
 import Layout from '../pages/components/MyLayout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const PostLink = props => (
-    <li>
-        <Link href={`/post?title=${props.title}`}>
-            <a>{props.title}</a>
-        </Link>
-    </li>
-);
-function Blog () {
-    return (
-        <Layout>
-            <h1>My Blog</h1>
-            <ul>
-                <PostLink title="Hello"/>
-                <PostLink title="World"/>
-            </ul>
-        </Layout>
-    );
+// const PostLink = props => (
+//     <li>
+//         <Link href={`/post?title=${props.title}`}>
+//             <a>{props.title}</a>
+//         </Link>
+//     </li>
+// );
+// function Blog () {
+//     return (
+//         <Layout>
+//             <h1>My Blog</h1>
+//             <ul>
+//                 <PostLink title="Hello"/>
+//                 <PostLink title="World"/>
+//             </ul>
+//         </Layout>
+//     );
+// }
+ const Index = props => (
+     <Layout>
+         <h1>Batman Tv Shows</h1>
+         <ul>
+             {props.shows.map(show => (
+                 <li key={show.id}>
+                     <Link href='/p/[i]' as="{`/p/${show.id}}">
+                         <a>{show.name}</a>
+                     </Link>
+                 </li>
+             ))}
+         </ul>
+     </Layout>
+     
+ );
+
+ Index.getInitialProps = async function(){
+         const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+         const data = await res.json();
+
+         console.log(`Show data fetched. Count: ${data.length}`);
+        return {
+            shows: data.map(entry => entry.show)
+        };
 }
 
-export default Blog;
+
+export default Index;
