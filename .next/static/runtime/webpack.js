3edabf5e8141f343bb3da5c33de8002858310e96
Geovanny Mero <jefferson_mero1911@hotmail.com>
2019-12-20 02:55:13 -0500

    p

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "055220591f95fd0db5f8";
+/******/ 	var hotCurrentHash = "67f03dd5175646d60d9e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
